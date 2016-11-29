94f70c394596673592511c7e9ab776eb4165457e
BLUE <nongsupisara58@gmail.com>
2016-11-29 14:51:29 +0700

    searchok

--- app/src/main/java/com/journaldev/searchview/MainActivity.java
+++ app/src/main/java/com/journaldev/searchview/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String[] wordStrings, detailStrings;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
